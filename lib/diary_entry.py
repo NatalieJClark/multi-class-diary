aaedaeb1fb0f16cebc66cd2cc7e28d6bd6cced3a
Natalie Clark <nj_clark@hotmail.co.uk>
2023-09-26 18:00:48 +0100

    Add DiaryEntry class

---
+++ lib/diary_entry.py
@@ -0,0 +1,49 @@
+from math import ceil
+
+class DiaryEntry:
+    # Public Properties:
+    #   title: a string
+    #   contents: a string
+
+    def __init__(self, title, contents): # title, contents are strings
+        # Side-effects:
+        #   Sets the title and contents properties
+        self.title = title
+        self.contents = contents
+        self._stop_off_point = 0
+
+    def count_words(self):
+        # Returns:
+        #   An integer representing the number of words in the contents
+        return len(self.contents.split())
+
+    def reading_time(self, wpm):
+        # Parameters:
+        #   wpm: an integer representing the number of words the user can read
+        #        per minute
+        # Returns:
+        #   An integer representing an estimate of the reading time in minutes
+        #   for the contents at the given wpm.
+        return ceil(self.count_words() / wpm)
+
+    def reading_chunk(self, wpm, minutes):
+        # Parameters:
+        #   wpm: an integer representing the number of words the user can read
+        #        per minute
+        #   minutes: an integer representing the number of minutes the user has
+        #            to read
+        # Returns:
+        #   A string representing a chunk of the contents that the user could
+        #   read in the given number of minutes.
+        # If called again, `reading_chunk` should return the next chunk,
+        # skipping what has already been read, until the contents is fully read.
+        # The next call after that it should restart from the beginning.
+        words = self.contents.split()
+        if self._stop_off_point >= len(words):
+            self._stop_off_point = 0
+        readable_chunk_length = wpm * minutes    
+        start_point = self._stop_off_point
+        end_point = self._stop_off_point + readable_chunk_length
+        readable_chunk = " ".join(words[start_point:end_point])
+        self._stop_off_point += readable_chunk_length
+        return readable_chunk
